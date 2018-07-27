75b5b4f21882f1b994196446fd6d63efcf22dccf
starlying <starlying@gmail.com>
2018-07-27 14:21:23 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using SiteServer.Plugin;
+
+namespace SS.HelloWorld
+{
+    public class Main : PluginBase
+    {
+        public override void Startup(IService service)
+        {
+            service.AddSiteMenu(siteId => new Menu
+            {
+                Text = "Hello World",
+                Href = "index.html"
+            });
+        }
+    }
+}
